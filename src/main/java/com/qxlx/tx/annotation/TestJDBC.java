97ca66e7a1edbee6f38a3aac4cee8b6561c9fcd2
qxlx <qxlxi@qq.com>
2024-12-17 22:40:16 +0800

    spring 事务

---
+++ src/main/java/com/qxlx/tx/annotation/TestJDBC.java
@@ -0,0 +1,17 @@
+package com.qxlx.tx.annotation;
+
+import java.sql.Connection;
+
+/**
+ * @author qxlx
+ * @date 2024/12/17 21:28
+ */
+public class TestJDBC {
+
+    public static void main(String[] args) {
+        Connection conn = null;
+
+//        conn.setTransactionIsolation(1);
+    }
+
+}
